a4495d774495a8213c926dfad495e035bf2665d8
victor <vvavulin@gmail.com>
2022-10-08 18:22:08 +0200

    Revert "added tests with hash colossion"
    This reverts commit dbf369372df148866f0fbb49296df43ac997075e.

--- src/homework/TestBinaryDict.java
+++ src/homework/TestBinaryDict.java
@@ -2,8 +2,6 @@ package homework;
 
 import java.util.List;
 
-import static java.util.Objects.hash;
-
 public class TestBinaryDict {
 
     public static void main(String[] args) {
@@ -29,37 +27,6 @@ public class TestBinaryDict {
         System.out.println("---------------Test filter() method----------------");
         List<String> filteredList = dict.filter( (v) -> v.length() > 3);
         System.out.println(filteredList);
-
-
-        System.out.println("---------------Test hash collision ----------------");
-        BinaryDictionary<String,String> dict1 = new BinaryDictionary<>();
-
-        System.out.println(hash("A".hashCode()));
-        System.out.println(hash("AaAa".hashCode()));
-        System.out.println(hash("BBBB".hashCode()));
-        System.out.println(hash("Aa".hashCode()));
-        System.out.println(hash("BB".hashCode()));
-        System.out.println(hash("AaBB".hashCode()));
-        System.out.println(hash("BBAa".hashCode()));
-
-
-        dict1.put("A", "String 1");
-        dict1.put("AaAa", "String 2");
-        dict1.put("BBBB", "String 3");
-        dict1.put("Aa", "String 4");
-        dict1.put("BB", "String 5");
-        dict1.put("AaBB", "String 6");
-        dict1.put("BBAa", "String 7");
-
-        System.out.println(dict1);
-        System.out.println(dict1.get("A"));
-        System.out.println(dict1.get("AaAa"));
-        System.out.println(dict1.get("BBBB"));
-        System.out.println(dict1.get("Aa"));
-        System.out.println(dict1.get("BB"));
-        System.out.println(dict1.get("AaBB"));
-        System.out.println(dict1.get("BBAa"));
-
 
     }
 
